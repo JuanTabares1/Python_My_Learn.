dfac1ac35090a9ee883b38f3bb70ed097eb43769
JuanTabares1 <juanjosetabarestrivino@gmail.com>
2024-11-17 10:29:40 -0500

    commit

--- archivosCSV/read.csv.py
+++ archivosCSV/read.csv.py
@@ -1,13 +1,13 @@
 import csv
 
 #Leer archivo
-""" with open('archivosCSV/products_updated.csv', mode='r') as file:
+""" with open('archivosCSV/products.csv', mode='r') as file:
     csv_reader = csv.DictReader(file)
     for row in csv_reader:
         print(row) """
 
 #Mostrar la informacion por columnas
-""" with open('archivosCSV/products_updated.csv', mode='r') as file:
+""" with open('archivosCSV/products.csv', mode='r') as file:
     csv_reader = csv.DictReader(file)
     for row in csv_reader:
         print(f"El producto: {row['name']}, Precio: {row['price']}") """
@@ -22,7 +22,24 @@ new_product = {
     'entry': '2024-07-01'
 }
 
-with open('archivosCSV/products_updated.csv', mode='a', newline='') as file:
+""" with open('archivosCSV/products.csv', mode='a', newline='') as file:
     file.write('\n')
     csv_writer = csv.DictWriter(file, fieldnames=new_product.keys())
-    csv_writer.writerow(new_product)
+    csv_writer.writerow(new_product) """
+
+#nueva colummna y crear una copia del archivo csv
+file_path = 'archivosCSV/products.csv'
+updated_file_path = 'archivosCSV/products_updated.csv'
+
+with open(file_path, mode='r') as file:
+    csv_reader = csv.DictReader(file)
+    #Obtener los nombres de las columnas existentes
+    fieldnames = csv_reader.fieldnames + ['total value']
+
+    with open(updated_file_path, mode='w', newline='') as updated_file:
+        csv_writer = csv.DictWriter(updated_file, fieldnames=fieldnames)
+        csv_writer.writeheader() #Escribir el encabezado
+
+        for row in csv_reader:
+            row['total_value'] = float(row['price']) * int(row['quantity'])
+            csv_writer.writerow(row)
